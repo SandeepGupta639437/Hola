b695bff9fd15e31e38fe03168ccf4dd5f2eb193e
vanshguptaji <vansh23164033@akgec.ac.in>
2024-11-12 00:15:14 +0530

    forgot Password , Otp validation page and reset Password page UI completed and linking of forgot password to their respective page done

--- app/src/main/java/com/example/hola/Login.kt
+++ app/src/main/java/com/example/hola/Login.kt
@@ -1,6 +1,11 @@
 package com.example.hola
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +16,44 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signIn)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val forgotPass: TextView = findViewById<TextView>(R.id.forgot_pass)
+        forgotPass.setOnClickListener{
+            val intent = Intent(this@Login, forgotPassword::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val pass_display : TextView = findViewById<TextView>(R.id.incorrectpass)
+        pass_display.visibility = View.INVISIBLE
+
+        val passwordEditText: EditText = findViewById(R.id.passwordEditText)
+        passwordEditText.transformationMethod = passwordTransformation()
+
     }
+}
+
+class passwordTransformation : PasswordTransformationMethod() {
+    override fun getTransformation(source: CharSequence, view: View): CharSequence {
+        return AsteriskCharSequence(source)
+    }
+
+    private class AsteriskCharSequence(private val source: CharSequence) : CharSequence {
+        override val length: Int
+            get() = source.length
+
+        override fun get(index: Int): Char {
+
+            return '*'
+        }
+
+        override fun subSequence(startIndex: Int, endIndex: Int): CharSequence {
+            return AsteriskCharSequence(source.subSequence(startIndex, endIndex))
+            }
+        }
 }
