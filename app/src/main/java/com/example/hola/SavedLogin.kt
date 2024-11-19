e21517ee46f0436746cace8c4d24560bc56aa114
vanshguptaji <vansh23164033@akgec.ac.in>
2024-11-20 03:06:25 +0530

    Saved Login Info Page Layout Done

---
+++ app/src/main/java/com/example/hola/SavedLogin.kt
@@ -0,0 +1,39 @@
+package com.example.hola
+
+import android.os.Bundle
+import android.widget.Switch
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SavedLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_saved_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val themeToggle: Switch = findViewById(R.id.themeToggle)
+
+        // Check the current theme and set the switch accordingly
+        themeToggle.isChecked =
+            AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+
+        // Listener for the toggle
+        themeToggle.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // Enable Dark Mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                // Enable Light Mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+    }
+}
