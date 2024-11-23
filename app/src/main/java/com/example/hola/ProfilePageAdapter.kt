ff15e90b74777164c2bf1fc24a80114667613f07
SandeepGupta639437 <sandeepguptag18@gmail.com>
2024-11-23 21:18:14 +0530

    live following recycle view resolved & grid recycle view in profile page to show user post is updated

---
+++ app/src/main/java/com/example/hola/ProfilePageAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.hola
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
+import com.squareup.picasso.Picasso
+
+class ProfilePageAdapter (private val context: Context, private val productArrayList: List<Product>) :
+    RecyclerView.Adapter<ProfilePageAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(context).inflate(R.layout.eachuserpost, parent, false)
+        return MyViewHolder(itemView)
+    }
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = productArrayList[position]
+        Picasso.get().load(currentItem.thumbnail).into(holder.Image)
+    }
+    override fun getItemCount(): Int {
+        return productArrayList.size
+    }
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var Image: ImageView = itemView.findViewById(R.id.userpost)
+    }
+}
