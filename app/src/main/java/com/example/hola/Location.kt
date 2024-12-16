36e6aa1e31851d47acfe5fa1f6fc046cad5b98ed
SandeepGupta639437 <sandeepguptag18@gmail.com>
2024-12-17 00:33:39 +0530

    app icon added and location page also added

---
+++ app/src/main/java/com/example/hola/Location.kt
@@ -0,0 +1,35 @@
+package com.example.hola
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class Location : AppCompatActivity(),OnMapReadyCallback{
+    private var mGoogleMap:GoogleMap?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_location)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val mapFragment=supportFragmentManager
+            .findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap=googleMap
+    }
+
+}
